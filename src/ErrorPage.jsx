186a06310fa6690573130aa2b24b2174949dec3c
David-Ndubuisi <david.ndubuisi006@gmail.com>
2024-12-24 00:45:24 +0100

    Initial commit

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+const ErrorPage = () => {
+  return (
+    <div className='errorpage'>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist. &#128566;</p>
+    </div>
+  )
+}
+
+export default ErrorPage
